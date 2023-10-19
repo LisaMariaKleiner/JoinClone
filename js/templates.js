fc33ffb9114d77e3c54c89a413f26262e6683314
Justin Kirsch <justin_kirsch@yahoo.com>
2023-10-19 02:41:53 +0200

    Added Hovereffect for Delete and Edit Button in Opened Add Task Card, Started to build a Dropdown for Assigned To in Opened task Card

--- js/templates.js
+++ js/templates.js
@@ -89,3 +89,36 @@ function createEditCard(
                   </div>
               </div>`;
   }
+
+  function createTask(element) {
+    return /*html*/ `
+          <div class="task_card" draggable="true" ondragstart="startDragging(${element.id})" onclick="openTaskDetailsCard(${element.id}, 'open')">
+  
+              <h3 class="user_story">User Story</h3>
+              <div class="task_information">
+                  <h2>${element.title}</h2>
+                  <p>${element.description}</p>
+              </div>
+              <div class="subtask_progress_container">
+                  <div class="subtask_progressbar">
+                      <div class="current_subtask_progress"></div>
+                  </div>
+                  <span>${element["subtaskCategory"]}</span>
+              </div>
+              <div class="task_footer">
+                  <div class="task_member_container">
+                      <div class="task_member first_member">
+                          <span>AM</span>
+                      </div>
+                      <div class="task_member second_member">
+                          <span>EM</span>
+                      </div>
+                  </div>
+                  <div class="task_urgency_information">
+  
+                  </div>
+              </div>
+          </div>
+  `;
+  }
+  
